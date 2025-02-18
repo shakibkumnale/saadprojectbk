0f2b8f3e20345e612d70faf0b407223b8a1a37ec
shakibkumnale <shakibkumnali@gmail.com>
2025-02-19 01:19:31 +0530

    cors  policy change 2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS || 'https://mahndi.vercel.app/',
+    origin: process.env.CORS || 'https://mahndi.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   })
